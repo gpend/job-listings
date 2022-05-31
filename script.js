f24b8a3041925bfd5fcc4d155571a4e2b97f035f
greg <gpendleton@gmail.com>
2022-05-31 15:04:52 -0500

    completed filtering

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-let selected = {
-    language: [],
-    tool: []
-}
+let selected = {} //TODO refactor to allowing multiple selections
 
 fetch("./data.json")
 .then(response => response.json())
@@ -60,13 +57,13 @@ function displayJobs (data){
     //add language to the job and add it to selected  state and to the header on click
     document.querySelectorAll(".job--language").forEach(element => {
         // @ts-ignore
-        element.addEventListener("click", (e)=> {handleJobClickMulti(e.target.innerText, 'language', data)})
+        element.addEventListener("click", (e)=> {handleJobClick(e.target.innerText, 'language', data)})
     })
 
     //add tool to the job and add it to selected and to the header on click
     document.querySelectorAll(".job--tool").forEach(element => {
         // @ts-ignore
-        element.addEventListener("click", (e)=> {handleJobClickMulti(e.target.innerText, 'tool', data)})
+        element.addEventListener("click", (e)=> {handleJobClick(e.target.innerText, 'tool', data)})
             
     })
 
@@ -84,41 +81,54 @@ function displayJobs (data){
 
 }
 
-function handleJobClick(text,type, data){
+function handleJobClick(text, type, data){
 
     selected[type] = text
     let html = `
-            <div class="selector__block">
+            <div class="selector__block  ${text}">
                 <div class="selector__block--text">${text}</div>
-                <div class="close_x">X</div>
+                <div class="close_x ${type} ${text}">X</div>
             </div>`
     
     document.querySelector(`.selector__${type}`).innerHTML = html
 
-    //filterData(data, toRemove)
-    //displayJobs(data)
+    document.querySelector(`.selector__${type}`).addEventListener("click", () => handleSelectorClose(event, data, text))
+    displayJobs(filterData(data))
 }
 
-function handleJobClickMulti(text, type, data){
-    if (!selected[type].includes(text)){ 
-        selected[type].push(text)
-        let html = selected[type].map(text => {
-            return `
-                <div class="selector__block">
-                    <div class="selector__block--text">${text}</div>
-                    <div class="close_x">X</div>
-                </div>`
-        }).join('')
+function handleSelectorClose(e, data, text){
+    const classes = e.target.classList
+    selected[classes[1]]  = "" //1 is type, 2 is text
+    document.querySelector(`.selector__block.${text}`).remove()
+    displayJobs(filterData(data))
+}
 
-        document.querySelector(`.selector__${type}`).innerHTML = html
+// function handleJobClickMulti(text, type, data){
+//     if (!selected[type].includes(text)){ 
+//         selected[type].push(text)
+//         let html = selected[type].map(text => {
+//             return `
+//                 <div class="selector__block">
+//                     <div class="selector__block--text">${text}</div>
+//                     <div class="close_x">X</div>
+//                 </div>`
+//         }).join('')
+
+//         document.querySelector(`.selector__${type}`).innerHTML = html
         
-        displayJobs(filterData(data, text, type))
-    }
-}
 
-function filterData(data, text, type){
-    type === 'language' ? type='languages' : ""
-    type === 'tool' ? type='tools' : ""
-    const filteredData = data.filter(job => job[type].includes(text))
+
+//         displayJobs(filterData(data, text, type))
+//     }
+// }
+
+function filterData(data){ 
+    console.log(selected)
+    let filteredData = data
+    selected.tool && (filteredData = filteredData.filter(job => job.tools.includes(selected.tool)))
+    selected.role && (filteredData = filteredData.filter(job => job.role.includes(selected.role)))
+    selected.level && (filteredData = filteredData.filter(job => job.level.includes(selected.level)))
+    selected.language && (filteredData = filteredData.filter(job => job.languages.includes(selected.language)))
+    console.log(filteredData)
     return filteredData
 }
